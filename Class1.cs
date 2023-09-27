2a18ef78150072d2888985da7e23b1dc2d1ffbfb
tssuru <115495515+tssuru@users.noreply.github.com>
2023-09-27 19:08:39 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace HW_3_OOP
+{
+    class Converter
+    {
+        private decimal _usd;
+        private decimal _eur;
+
+        public Converter(decimal usd, decimal eur)
+        {
+            _usd = usd;
+            _eur = eur;
+        }
+
+        public decimal UahToUsd(decimal uahAmount)
+        {
+            return uahAmount / _usd;
+        }
+
+        public decimal UahToEur(decimal uahAmount)
+        {
+            return uahAmount / _eur;
+        }
+
+        public decimal UsdToUah(decimal usdAmount)
+        {
+            return usdAmount * _usd;
+        }
+
+        public decimal EurToUah(decimal eurAmount)
+        {
+            return eurAmount * _eur;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using HW_3_OOP;
+
+var converter = new Converter(36.96m, 38.9m);
+
+Console.WriteLine("Converting 100 UAH to USD: " + converter.UahToUsd(100m));
+Console.WriteLine("Converting 100 UAH to EUR: " + converter.UahToEur(100m));
+Console.WriteLine("Converting 10 USD to UAH: " + converter.UsdToUah(10m));
+Console.WriteLine("Converting 10 EUR to UAH: " + converter.EurToUah(10m));
+       
